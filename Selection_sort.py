8be40517dfd7a4a5a1dbd75f6766088ac0237f63
sanjayng007 <sanjayng007@gmail.com>
2017-09-29 16:38:00 +0530

    Update and rename selection_sort.py to Selection_sort.py

--- Selection_sort.py
+++ Selection_sort.py
@@ -1,5 +1,15 @@
+"""
+    Selection Sort
+      Properties:
+       Not stable
+       O(1) extra space
+       Θ(n2) comparisons
+       Θ(n) swaps
+       Not adaptive
+"""
 #to use comma seperated input uncomment below line
 #x = [int(i) for i in input().strip().split(",")]
+
 x=[10,9,8,7,6,5,4,3,2,1]
 for i in range(len(x)-1):
     minn = i
